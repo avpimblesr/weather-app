ba53e160c36d90f6eddc2459b9571feb8f74f417
avpimblesr <allan.pimble@gmail.com>
2020-12-04 12:17:16 -0500

    Converted LoginForm to class component

--- client/src/components/LoginForm.jsx
+++ client/src/components/LoginForm.jsx
@@ -1,20 +1,39 @@
 import React from 'react'
 
 
-export default function LoginForm() {
-  return (
-    <div className="container">
-      <form>
-        <h2>Log In</h2><br />
-        <label htmlFor="email">Email</label><br />
-        <input type="email" name="email" /><br />
+class LoginForm extends React.Component {
+  constructor (props) {
+    super(props)
+    this.state = ({
+      email:'',
+      password:''
+    })
+  }
+  
+  render() {
+    return (
+      <div className="container">
+        <form>
+          <h3>Log In</h3><br />
+          <section className="form-group">
+            <label>Email:</label>
+            <input type="email"
+              className="form-control" />
+          </section>
 
-        <label htmlFor="password">Password</label><br />
-        <input type="password" name="password" /><br /><br />
+          <section className="form-group">
+            <label>Password:</label>
+            <input type="password"
+              className="form-control" />
+          </section>
 
-      <button type="submit">Submit</button>
-      </form><br />
-      <p>Not registered?&nbsp; <a href="#">Register</a></p>
-    </div>
-  )
+
+          <button className="btn btn-primary" type="submit">Submit</button>
+        </form><br />
+        <p>Not registered?&nbsp; <a href="javascript void(0)">Register</a></p>
+      </div>
+    )
+
+  }
 }
+export default LoginForm;
