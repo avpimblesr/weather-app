a56b6d426712eb0bf69cfe8b6eae0fef6d7553d4
avpimblesr <allan.pimble@gmail.com>
2020-12-04 12:38:17 -0500

    Added bindings to LoginForm

--- client/src/components/LoginForm.jsx
+++ client/src/components/LoginForm.jsx
@@ -8,23 +8,57 @@ class LoginForm extends React.Component {
       email:'',
       password:''
     })
+    this.onEmailChange = this.onEmailChange.bind(this)
+    this.onPasswordChange =this.onPasswordChange.bind(this)
+    this.onFormSubmitted = this.onFormSubmitted.bind(this)
   }
-  
+
+  onComponentDidMount() {
+    console.log("Component just mounted")
+  }
+
+  onComponentDidUpdate() {
+    console.log("Component just updated")
+  }
+
+  onEmailChange (e) {
+    this.setState({
+      email: e.target.value
+    })
+  }
+
+  onPasswordChange (e) {
+    this.setState({
+      password: e.target.value
+    })
+  }
+
+onFormSubmitted (e) {
+  e.preventDefault()
+  console.log("Form has been submitted")
+}
+
   render() {
     return (
       <div className="container">
-        <form>
+        <form onSubmit={this.onFormSubmitted}>
           <h3>Log In</h3><br />
           <section className="form-group">
             <label>Email:</label>
             <input type="email"
-              className="form-control" />
+              className="form-control" 
+              value={this.state.email}
+              onChange={this.onEmailChange}
+              />
           </section>
 
           <section className="form-group">
             <label>Password:</label>
             <input type="password"
-              className="form-control" />
+              className="form-control" 
+              value={this.state.password}
+              onChange={this.onPasswordChange}
+              />
           </section>
 
 
